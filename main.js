4103fd7dcaaeba0604396e6cf4690c33d60ff6da
Sravya-idk-lol <79676503+Sravya-idk-lol@users.noreply.github.com>
2022-02-17 21:00:32 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+var mouseEvent = "empty";
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+var color = "black";
+var width = 5;
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e) {
+    color = document.getElementById("color").value;
+    width = document.getElementById("width").value;
+    mouseEvent = "mouseDown";
+}
+canvas.addEventListener("mouseup", my_mousedown);
+
+function my_mouseup(e) {
+    mouseEvent = "mouseup";
+}
+
+canvas.addEventListener("mouseleave", my_mouseleave);
+
+function my_mouseleave(e){
+    mouseEvent = "mouseleave";
+}
+
+canvas.addEventListener("mousemove", my_mousemove);
+
+function my_mousemove(e);{
+current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+
+if (mouseEvent == "mouseDown"){
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width_of_line;  
+    ctx.moveTo(last_position_of_touch_x, last_position_of_touch_y);
+    ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
+    ctx.stroke();
+
+    last_position_of_touch_x = current_position_of_touch_x;
+    last_position_of_touch_y = current_position_of_touch_y;   
+} 
+
+   
+
+canvas.addEventListener("touchstart", my_touchstart)
+
+function my_touchstart(e){
+    last_position_of_x = current_position_of_mouse_x; 
+    last_position_of_y = current_position_of_mouse_y;
+}
+
+canvas.addEventListener("touchmove", my_touchmove);
+
+function my_touchmove(e){
+    last_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft; 
+    last_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
+    ctx.beginPath();
+    ctx.stokeStyle - color;
+    ctxlineWidth - width_of_line;
+    ctx.moveTo(last_position_of_touch_x, last_position_of_touch_y);
+    ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
+    ctx.stroke();
+
+    last_position_of_touch_x = current_position_of_touch_x;
+    last_position_of_touch_y = current_position_of_touch_y;
+}
